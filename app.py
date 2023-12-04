dbf6cd8649462cff48f7acf2d0b0d42daa446881
VannWasHere <ateen1203@gmail.com>
2023-12-04 12:26:43 +0700

    Add INDEX HTML

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
+from Models.DiWordDetector import DiWordDetector
 
 app = Flask(__name__, template_folder='Pages')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/detect', methods=['POST'])
+def detect_diword():
+    if request.method == 'POST':
+        paragraph = request.form.get('paragraph')
+        detector = DiWordDetector()
+        result = detector.detect_di_usage(paragraph)
+        return render_template('index.html', result=result, paragraph=paragraph)
 
 if __name__ == '__main__':
     app.run(debug=True)
