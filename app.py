5e5cda3d189dc6b5e46f8d4fdd372bf6e1fdc1c6
VannWasHere <ateen1203@gmail.com>
2023-12-18 09:53:56 +0700

    Fix App.py

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ def index():
     paragraph_terikat = None
     result_majemuk_final = []
     paragraph_majemuk = []
-    return render_template('index.html', result_di=result_di, paragraph_di=paragraph_di,
+    return render_template('index.html', 
+                           result_di=result_di, paragraph_di=paragraph_di,
+                           result_terikat=result_terikat, paragraph_terikat=paragraph_terikat,
                            result_majemuk_final=result_majemuk_final, paragraph_majemuk=paragraph_majemuk)
 
 @app.route('/detect', methods=['POST'])
@@ -206,8 +208,42 @@ def detect_diword():
                     'suggestion' : kataKoreksi[i]
                 }
             i += 1
-        return render_template('index.html', result_di=result_di, paragraph_di=paragraph,
-                               result_majemuk_final=result_majemuk_final, paragraph_majemuk=paragraph)
+            
+         # Ambil data paragraf
+        paragraph = request.form.get('paragraph')
+
+        # Deteksi Kata DI
+        detector = DiWordDetector()
+        result_di = detector.detect_di_usage(paragraph)
+
+        # Detect Kata Terikat        
+        with open('Models/deteksi_terikat/kata-dasar.txt', 'r') as file:
+            word_list = [line.strip() for line in file.readlines()]        
+        preprocess = preprocessing(paragraph)
+
+        # RabinKarp untuk cari pattern data input
+        rbnKarp = rabinKarp(preprocess, patterns)
+
+        # Sort hasil dari index terkecil
+        sort_data = sorted(rbnKarp, key=lambda x: x[-1])
+
+        # Remove duplicates
+        unique_data = {item[2]: item for item in sort_data}.values()
+
+        # Deteksi bigram
+        bigrams = generate_bigrams(preprocess)
+        deteksi_bigram_terikat = []
+
+        # Cari pattern di bigram
+        for word, index in bigrams:
+            result_terikat = rabinKarp(word, patterns, True, index)
+            if result_terikat != []:
+                deteksi_bigram_terikat.append(result_terikat)
+
+        result_terikat_dict = {}
+
+        result_terikat_dict.update(validateSatuKata(unique_data, word_list))
+            
         if deteksi_bigram_terikat:
             result_terikat_dict.update(validateDuaKata(deteksi_bigram_terikat, word_list, paragraph))
             
@@ -215,9 +251,9 @@ def detect_diword():
             result_terikat_dict = {'errormessage': "Tidak ada kata terikat yang ditemukan"}
 
         return render_template('index.html', result_di=result_di, paragraph_di=paragraph, 
-                               result_terikat=result_terikat_dict, paragraph_terikat=paragraph
+                               result_terikat=result_terikat_dict, paragraph_terikat=paragraph,
+                               result_majemuk_final=result_majemuk_final, paragraph_majemuk=paragraph
                                )
-
 
 @app.route('/about')
 def about():
@@ -228,4 +264,4 @@ def team():
     return render_template('team.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
